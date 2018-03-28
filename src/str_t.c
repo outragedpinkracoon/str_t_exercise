93920bda43ed9167d9b218e0af84e99f164887d8
Scott Riley <scott.riley111@gmail.com>
2018-03-28 13:49:16 +0100

    Implements first 3 functions

--- src/str_t.c
+++ src/str_t.c
@@ -1,8 +1,47 @@
 #include "str_t.h"
 
+#include <stdlib.h>
+#include <string.h>
+
 const char * const message = "Hello, World!";
 
 const char *hello(void)
 {
     return message;
+}
+
+str_t* str_new_from_raw_parts(const char* ptr, size_t len)
+{
+    str_t *result = malloc(sizeof(str_t));
+    result->len = len;
+    result->ptr = ptr;
+
+    return result;
+}
+
+void str_free(str_t* p_str)
+{
+    free(p_str);
+}
+
+str_t* str_new_from_zero_terminated(const char* ptr)
+{
+    size_t length = strlen(ptr);
+    return str_new_from_raw_parts(ptr, length);
+}
+
+void str_copy(const str_t *p_source, str_t *p_destination)
+{
+    const char *str = p_source->ptr;
+
+    size_t source_length = p_source->len;
+
+    size_t buffer_size = source_length + 1;
+
+    char *buffer = malloc(buffer_size);
+
+    strncpy(buffer, str, buffer_size);
+
+    p_destination->ptr = buffer;
+    p_destination->len = source_length;
 }
--- tests/test_str_t.c
+++ tests/test_str_t.c
@@ -16,11 +16,42 @@ void test_hello(void)
    TEST_ASSERT_EQUAL_STRING("Hello, World!", hello());
 }
 
+void test_str_new_from_raw_parts(void)
+{
+  char *input_string = "Sandwich";
+  int input_length = 8;
+  str_t *output = str_new_from_raw_parts(input_string, input_length);
+  TEST_ASSERT_EQUAL_UINT(8, output->len);
+  TEST_ASSERT_EQUAL_STRING("Sandwich", output->ptr);
+  str_free(output);
+}
+
+void test_str_new_from_zero_terminated(void)
+{
+  char *input_string = "Wrap";
+  str_t *output = str_new_from_zero_terminated(input_string);
+  TEST_ASSERT_EQUAL_UINT(4, output->len);
+  TEST_ASSERT_EQUAL_STRING("Wrap", output->ptr);
+  str_free(output);
+}
+
+void test_str_copy(void)
+{
+  str_t *input = str_new_from_zero_terminated("Banana");
+  str_t output;
+  str_copy(input, &output);
+  TEST_ASSERT_EQUAL_STRING("Banana", output.ptr);
+  TEST_ASSERT_EQUAL_UINT(6, output.len);
+}
+
 int main(void)
 {
    UnityBegin("tests/test_str_t.c");
 
    RUN_TEST(test_hello);
+   RUN_TEST(test_str_new_from_raw_parts);
+   RUN_TEST(test_str_new_from_zero_terminated);
+   RUN_TEST(test_str_copy);
 
    UnityEnd();
 
